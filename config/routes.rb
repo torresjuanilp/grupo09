d1cedd63f59b01dd746d9c47f8156418c198b2d4
camila873 <wpgnct@gmail.com>
2017-11-14 15:11:21 -0300

    Categorías
    Creación y eliminación

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :questions
+  resources :categories
+ 
+get "questions/:id" => "posts#show"
+get "questions" => "questions#index"
+get "categories" => "categories#index"
+get "categories/new" => "categories#new"
+get "categories/show0" => "categories#show"
   devise_for :users
     root 'main#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108162645) do
+ActiveRecord::Schema.define(version: 20171114150408) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_questions", id: false, force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "titulo"
+    t.string "descripcion"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
