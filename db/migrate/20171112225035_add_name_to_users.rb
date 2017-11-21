6652b1b7cb6bf0a3a3ab5ff76e5aa8f3905cdccf
Elias Caceres <elias_cipo@hotmail.com>
2017-11-21 09:45:54 -0300

    error menor

--- db/migrate/20171112225035_add_name_to_users.rb
+++ db/migrate/20171112225035_add_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :name, :stng
+    add_column :users, :name, :string
     add_column :users, :apellido, :string
     add_column :users, :facultad, :string
   end
