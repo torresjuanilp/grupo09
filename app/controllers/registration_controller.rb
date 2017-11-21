2f87d1accefd3519ce90d11af446d4c78dc87dde
Elias Caceres <elias_cipo@hotmail.com>
2017-11-21 09:55:18 -0300

    cambio de metodo

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_action :configure_permitted_parameters, :only => [:create]
   private
   def current_user?(user)
   	@current_user == user
