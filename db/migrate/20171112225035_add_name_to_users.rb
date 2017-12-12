585f0caa32385e9ada860f184208aa25baa8f501
Juan Torres <torresjuanilp@gmail.com>
2017-12-12 14:30:14 -0300

    Asignar facultad

--- db/migrate/20171112225035_add_name_to_users.rb
+++ db/migrate/20171112225035_add_name_to_users.rb
@@ -2,6 +2,6 @@ class AddNameToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :name, :string
     add_column :users, :apellido, :string
-    add_column :users, :facultad, :string
+    add_column :users, :faculty_id, :integer
   end
 end
