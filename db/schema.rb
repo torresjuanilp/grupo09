7f933688d1eb0b409716f74aa7421b02b6f7142e
Juan Torres <torresjuanilp@gmail.com>
2017-12-13 12:55:45 -0300

    Cambio menor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213135644) do
+ActiveRecord::Schema.define(version: 20171213142229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171213135644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "question_count", default: 0
   end
 
   create_table "categories_questions", id: false, force: :cascade do |t|
